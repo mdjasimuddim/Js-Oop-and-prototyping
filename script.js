2846c17fad77598373fed77f97268fae64052c59
jasim <mdjasimuddimruet@gmail.com>
2023-03-26 23:44:29 +0600

    oop and protyping

---
+++ script.js
@@ -0,0 +1,57 @@
+//primitive data type -> Number, object, boolean, undefined, null
+//object data type -> array, date, function, string/number/boelan er wrapper
+
+
+// var a = 10, b = 20;
+
+// console.log('Before swap: Value of a: ' + a + ' and value of b: ' + b);
+
+// function swap(a, b) {
+//    console.log('Before Swap inside function: Value of a: ' + a + ' and value of b: ' + b);
+//    var temp = a;
+//    a = b;
+//    b = temp;
+//    console.log('After Swap inside function: Value of a: ' + a + ' and value of b: ' + b);
+// }
+
+// swap(a, b);
+// console.log('After swap: Value of a: ' + a + ' and value of b: ' + b);
+
+
+
+// var obj = {
+//     a: 10,
+//     b: 20
+//  };
+ 
+//  console.log('Before swap: Value of a: ' + obj.a + ' and value of b: ' + obj.b);
+ 
+//  function swap(x) {
+//     console.log('Before Swap inside function: Value of a: ' + x.a + ' and value of b: ' + x.b);
+//     var temp = x.a;
+//     x.a = x.b;
+//     x.b = temp;
+//     console.log('After Swap inside function: Value of a: ' + x.a + ' and value of b: ' + x.b);
+//  }
+ 
+//  swap(obj);
+//  console.log('After swap: Value of a: ' + obj.a + ' and value of b: ' + obj.b);
+
+
+// //  _proto_ k dander proto bole 
+
+// var one_arr = [1, 2, 3, 4, 5];
+// console.dir(one_arr);
+
+
+// var str = "My Name is md jasim uddim";
+// console.log(str);
+
+// var str = new String('My name is md jasim uddim');
+// console.dir(str);
+
+
+// wrapper function 
+// new String()
+// new Number()
+// new Boolean()
